97eeddfe00d21808f7eb1ae60a792d1075e523c5
ngocphan1995 <ngocphan12031995@gmail.com>
2016-06-08 14:46:38 +0700

    commit 4.0.29 va file bakery.sql

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2016 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Fri, 03 Jun 2016 02:02:19 GMT
+ */
+
+if ( ! defined( 'NV_MAINFILE' ) )
+{
+	die( 'Stop!!!' );
+}
+
+$db_config['dbhost'] = '127.0.0.1';
+$db_config['dbport'] = '';
+$db_config['dbname'] = 'demoBakery';
+$db_config['dbsystem'] = 'demoBakery';
+$db_config['dbuname'] = 'root';
+$db_config['dbpass'] = '';
+$db_config['dbtype'] = 'mysql';
+$db_config['collation'] = 'utf8mb4_unicode_ci';
+$db_config['charset'] = 'utf8mb4';
+$db_config['persistent'] = false;
+$db_config['prefix'] = 'nv4';
+
+$global_config['site_domain'] = '';
+$global_config['name_show'] = 0;
+$global_config['idsite'] = 0;
+$global_config['sitekey'] = 'a7fd3a0d1a7dbe745738f304b96303ca';// Do not change sitekey!
+$global_config['hashprefix'] = '{SSHA}';
+$global_config['cached'] = 'files';
+$global_config['extension_setup'] = 3; // 0: No, 1: Upload, 2: NukeViet Store, 3: Upload + NukeViet Store
